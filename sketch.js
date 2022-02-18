ed81d8e9ff25da81b74195d8a903b1519d018715
BrenoPlayer <99928529+BrenoPlayer@users.noreply.github.com>
2022-02-17 21:30:11 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var teste = "Breno";
+
+function setup() {
+  console.log(teste);
+  createCanvas(400,400);
+}
+var bol = createSprite(200, 200, 20, 20);
+function draw() 
+{
+  background("whte");
+ drawSprites();
+}
+
+
+
+
